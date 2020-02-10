186b1709f4ea75be5554d6fefb5714ee162145ba
son2412 <ngovanson196@gmail.com>
2020-02-10 17:11:37 +0700

    email config

---
+++ src/app/Email/CustomEmail.ts
@@ -0,0 +1,128 @@
+import nodemailer from "nodemailer";
+import Style from "./Style";
+import * as _ from "lodash";
+
+export default class CustomEmail {
+  private content: string;
+  private customGreetingStyle: any;
+  private customSubjectStyle: any;
+  public from_where: string;
+  public from_name: string;
+  public subject_email: string;
+  public greeting_email: string;
+  to_name: string;
+  constructor() {
+    this.content = "";
+    this.customGreetingStyle = null;
+    this.customSubjectStyle = null;
+    this.from_where = process.env.DEFAULT_SENDER_EMAIL;
+    this.from_name = process.env.DEFAULT_SENDER;
+  }
+
+  to(to, name = "") {
+    if (_.isNil(to)) {
+      throw new Error("receiver's email is required");
+    }
+    this.to = to;
+    this.to_name = name;
+    return this;
+  }
+
+  from(from, name = "") {
+    if (_.isNil(from)) {
+      throw new Error("sender's email is required");
+    }
+    this.from = from;
+    this.from_name = name;
+    return this;
+  }
+
+  subject(subject: string = "", customStyle: any = null) {
+    this.customSubjectStyle = customStyle;
+    this.subject_email = subject;
+    return this;
+  }
+
+  greeting(greeting = "", customStyle = null) {
+    this.customGreetingStyle = customStyle;
+    this.greeting_email = greeting;
+    return this;
+  }
+
+  line(text = "", customStyle = null) {
+    this.content += _.isNil(customStyle)
+      ? `<p style="${Style.paragraph}">${text}</p>`
+      : `<p style="${Style.paragraph} ${customStyle}">${text}</p>`;
+    return this;
+  }
+
+  section(data = "", customStyle = null) {
+    this.content += _.isNil(customStyle)
+      ? `<div>${data}</div>`
+      : `<div style="${customStyle}">${data}</div>`;
+    return this;
+  }
+
+  action(text = "", link = "#", type = "default") {
+    this.content += `<div style="text-align:center">
+            <a href="${link}">
+                <button style="${Style.button} ${Style.button_default}" class="${type}">${text}</button>
+            </a>    
+        </div>`;
+    return this;
+  }
+
+  buildContent() {
+    let html = `<html><body style="${Style.body}">`;
+    html += `<div style="${Style.header}">
+                <div style="${Style.avt}">
+                </div>
+                <div style="${Style.logo}">
+                  <img style="${Style.logo_img}" src="">
+                </div>
+              </div>`;
+    html += _.isNil(this.customSubjectStyle)
+      ? `<h1 style="${Style.subject}">${this.subject}</h1>`
+      : `<h1 style="${Style.subject} ${this.customSubjectStyle}">${this.subject}</h1>`;
+    html += _.isNil(this.customGreetingStyle)
+      ? `<div style="${Style.greeting}">${this.greeting}</div>`
+      : `<div style="${Style.greeting} ${this.customGreetingStyle}">${this.greeting}</div>`;
+    html += `<div style="${Style.content}">${this.content}</div>`;
+    html += `<p style="${Style.footer}">&copy; Copyright by Grass</p>`;
+    html += "</body></html>";
+    return html;
+  }
+
+  send() {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: process.env.EMAIL_SECURE === "ssl",
+      // ignoreTLS: true,
+      auth: {
+        user: process.env.EMAIL_USER, // generated ethereal user
+        pass: process.env.EMAIL_PASSWORD // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: true
+      }
+    });
+
+    return transporter.sendMail(
+      {
+        from: process.env.EMAIL_USER,
+        to: this.to,
+        subject: this.subject,
+        html: this.buildContent()
+      },
+      (err, info) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Message sent: " + info.messageId);
+          transporter.close();
+        }
+      }
+    );
+  }
+}
---
+++ src/app/Email/Style.ts
@@ -0,0 +1,70 @@
+export default {
+  body: `
+          max-width: 700px;
+          font-family: 'Arial';
+          margin: 15px auto;
+      `,
+  header: `
+          display: flex;
+      `,
+  avt: `
+          width: 50%;
+      `,
+  avt_img: `
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+      `,
+  logo: `
+          width: 50%
+      `,
+  logo_img: `
+          float: right;
+      `,
+  subject: `
+            font-size: 36px;
+            color: #333;
+            margin-bottom: 40px;
+            margin-top: 15px;
+        `,
+  greeting: `
+            color: red;
+            font-weight: 600;
+        `,
+  paragraph: `
+            font-size: 14px;
+            margin-bottom: 20px;
+        `,
+  button: `
+            display: inline-block;
+            padding: 10px 25px;
+            margin-bottom: 0;
+            font-size: 14px;
+            font-weight: normal;
+            line-height: 1.42857143;
+            white-space: nowrap;
+            vertical-align: middle;
+            -ms-touch-action: manipulation;
+                touch-action: manipulation;
+            cursor: pointer;
+            text-align: left;
+            -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                    user-select: none;
+            background: #FF7700 !important;
+            color: #fff !important;
+            border: 1px solid transparent;
+            border-radius: 25px;
+            font-weight: 600;
+            `,
+  button_default: `
+            color: #333;
+            border-color: transparent;
+            `,
+  footer: `
+          text-align: center;
+          font-size: 14px;
+      `,
+  content: ``
+};
