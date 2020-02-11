70e18234278642883db14b5a2528d798ac7aadff
son2412 <ngovanson196@gmail.com>
2020-02-11 14:25:31 +0700

    send sms nexmo

---
+++ src/app/Services/Sms.ts
@@ -0,0 +1,52 @@
+import Nexmo from 'nexmo';
+import * as _ from 'lodash';
+
+export class SendSms {
+  options: any;
+  enabled: boolean;
+  nexmo: any;
+  constructor() {
+    if ((process.env.NEXMO_ENABLED as any) === true || process.env.NEXMO_ENABLED === 'true') {
+      this.enabled = true;
+      if (_.isNil(process.env.NEXMO_API_KEY)) {
+        throw new Error('Missing configuration for NEXMO_API_KEY');
+      }
+      if (_.isNil(process.env.NEXMO_API_SECRET)) {
+        throw new Error('Missing configuration for NEXMO_API_SECRET');
+      }
+      this.options = {
+        apiKey: process.env.NEXMO_API_KEY,
+        apiSecret: process.env.NEXMO_API_SECRET,
+      };
+    } else {
+      this.enabled = false;
+    }
+  }
+
+  isEnabled() {
+    return this.enabled;
+  }
+
+  getInstance() {
+    if (this.isEnabled()) {
+      if (_.isUndefined(this.nexmo)) {
+        this.nexmo = new Nexmo(this.options);
+      }
+      return this.nexmo;
+    }
+  }
+
+  send(receiver: string, text: string) {
+    this.getInstance().message.sendSms(process.env.NEXMO_SENDER, receiver, text, { type: 'unicode' }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (data.messages[0]['status'] === '0') {
+          console.log('Message sent successfully.');
+        } else {
+          console.log(`Message failed with error: ${data.messages[0]['error-text']}`);
+        }
+      }
+    });
+  }
+}
