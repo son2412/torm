c747623ac893fb3e9621d8cf8c65074cbaf8a8d4
son2412 <ngovanson196@gmail.com>
2020-02-20 11:03:23 +0700

    pkg fb

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { UserRepository } from "../app/Repositories";
 import { SendWelcomeEmailNotification } from "../app/Notifications/SendWelcomeEmailNotification";
+import Auth from "../app/Services/Auth";
 
 export class UserController {
   async all(request: Request, response: Response) {
     const result = await new UserRepository().getAll();
-    new SendWelcomeEmailNotification(result[0]).sendMail();
+    // new SendWelcomeEmailNotification(result[0]).sendMail();
     response.json(result);
   }
 }
