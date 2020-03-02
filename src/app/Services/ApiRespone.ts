30fd59c254ad48338d025e62923cf56d2ddb0158
son2412 <ngovanson196@gmail.com>
2020-03-02 18:06:42 +0700

    response paginate

--- src/app/Services/ApiRespone.ts
+++ src/app/Services/ApiRespone.ts
@@ -7,7 +7,15 @@ export class ApiRespone {
     return { data };
   }
 
-  static paginate() {}
+  static paginate(data: any, param?: any) {
+    return {
+      data: data[0],
+      totalRow: data[1],
+      totalPage: Math.ceil(data[1] / param.pageSize),
+      currentPage: param.pageIndex,
+      perPage: param.pageSize
+    };
+  }
 
   static success() {
     return { success: true };
