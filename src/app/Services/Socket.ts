b9cdad589135616c48307117df02d2224d533454
son2412 <ngovanson196@gmail.com>
2020-12-04 09:24:26 +0700

    soket change

--- src/app/Services/Socket.ts
+++ src/app/Services/Socket.ts
@@ -1,12 +1,22 @@
 const socketio = require('socket.io');
 import * as auth from '../../app/Middleware/AuthMiddleware';
 import { User } from '@entity/index';
+export enum chanelName {
+  REALTIME = '/realtime'
+}
+export enum SocketEvent {
+  SERVER_SEND_MESSAGE = 'server-send-message',
+  CLIENT_SEND_MESSAGE = 'client-send-message',
+  USER_JOIN_ROOM = 'user-join-room',
+  ROOM_COUNT_USER = 'room-count-user',
+  USER_LEAVE_ROOM = 'user-leave-room'
+}
 let io = null;
 export function socket(server) {
   try {
     if (!io) {
       io = socketio(server);
-      const nsp = io.of('/real-time');
+      const nsp = io.of(chanelName.REALTIME);
       nsp.on('connection', async function (socket: any) {
         // console.log(`${socket.id} connected`);
         const token = socket.handshake.query.token;
@@ -22,17 +32,45 @@ export function socket(server) {
         }
         user.isOnline = true;
         user.save();
-        // socket.on('client-send-message', function(message: any) {
-        //   console.log(message);
-        //   nsp.to(1).emit('server-send-message', message);
-        // });
-        // socket.on('JoinRoom', (roomId) => {
-        //   socket.join(roomId);
-        //   console.log(`${socket.id} joined room ${roomId}!`);
-        //   socket.on('client-send-message', function (message: any) {
-        //     nsp.to(roomId).emit('server-send-message', message);
-        //   });
-        // });
+        socket.on(SocketEvent.USER_JOIN_ROOM, function (data: { targetId: number; type: string }) {
+          if (!data) {
+            return;
+          }
+          const roomName = `any`;
+          socket.join(roomName);
+          const countMember = getRoomConnections(chanelName.REALTIME, roomName);
+          createEvent(chanelName.REALTIME, roomName, {
+            data: { countMember: countMember },
+            event: SocketEvent.ROOM_COUNT_USER
+          });
+        });
+        socket.on(SocketEvent.USER_LEAVE_ROOM, function (data: { targetId: number; type: string }) {
+          if (!data) {
+            return;
+          }
+          const roomName = `any`;
+          socket.leave(roomName);
+
+          setTimeout(() => {
+            const countMember = getRoomConnections(chanelName.REALTIME, roomName);
+            createEvent(chanelName.REALTIME, roomName, {
+              data: { countMember: countMember },
+              event: SocketEvent.ROOM_COUNT_USER
+            });
+          }, 200);
+        });
+        socket.on(SocketEvent.CLIENT_SEND_MESSAGE, function (data: { message: string }) {
+          if (!data) {
+            return;
+          }
+          const roomName = `any`
+          setTimeout(() => {
+            createEvent(chanelName.REALTIME, roomName, {
+              data: data,
+              event: SocketEvent.SERVER_SEND_MESSAGE
+            });
+          }, 200);
+        });
         socket.on('disconnect', async () => {
           const user = await User.findOne({ where: { id: decode.data.id } });
           user.isOnline = false;
@@ -45,4 +83,24 @@ export function socket(server) {
   } catch (error) {
     console.log(error);
   }
+}
+
+export function createEvent(namespace, room, payload: { data: any; event: string }, callback?) {
+  const { data, event } = payload;
+  io.of(namespace).to(room).emit(event, data);
+  if (typeof callback === 'function') {
+    callback();
+  }
+}
+
+function getRoomConnections(nameSpace, roomName) {
+  return io.nsps[nameSpace].adapter.rooms[roomName] ? io.nsps[nameSpace].adapter.rooms[roomName].length : 0;
+}
+
+export function getLength(namespace, room) {
+  let roomVideo = io.of(namespace).to(room).adapter.rooms[room];
+  if (!roomVideo) {
+    return 0;
+  }
+  return roomVideo.length;
 }
