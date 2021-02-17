b2e9cca84d9d9c748ea934446a8b5448f222c8cc
son2412 <ngovanson196@gmail.com>
2021-02-17 17:14:36 +0700

    test ci

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 echo "Changing directory to ~/torm"
 cd torm
 echo "Pulling from git"
-git pull
+git pull origin dev
 echo "Install"
 npm install
+echo "Build"
+npm run build
 echo "Restarting pm2 instances"
 pm2 restart all
